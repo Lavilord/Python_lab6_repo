5a74321a6d87dfe41243049d7c7db813df5fb513
Lavilord <74894820+Lavilord@users.noreply.github.com>
2021-04-15 09:38:19 +0300

    code for lab6

---
+++ lab6/animal.py
@@ -0,0 +1,8 @@
+
+class Animal:
+    def __init__(self, weight_in_grams: int, price: int, origin_country: str, is_predator: bool, eats_in_grams: int):
+        self.weight_in_grams = weight_in_grams
+        self.price = price
+        self.origin_country = origin_country
+        self.is_predator = is_predator
+        self.eats_in_grams = eats_in_grams
